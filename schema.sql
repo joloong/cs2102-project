4bce16f5cf9317ad040eb66e41f269dce8437514
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2021-03-30 23:07:17 +0800

    add Employees table to schema

--- schema.sql
+++ schema.sql
@@ -1,1 +1,14 @@
 -- CS2102 Project Team 41 schema.sql
+
+CREATE TABLE IF NOT EXISTS Employees (
+    sid char(20) primary key,
+    name text not null,
+    phone text not null,
+    email text not null,
+    join_date date not null,
+    address text not null,
+    depart_date date
+
+    constraint valid_join_depart_date check ((depart_date - join_date) >= 0);
+);
+
