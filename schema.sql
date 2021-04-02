15e72dea99bf51f76b562fd9c0c78a715e02d763
MackyMaguire <zhoutianyu68@gmail.com>
2021-04-03 02:58:27 +0800

    Added Tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,22 @@
 -- CS2102 Project Team 41 schema.sql
 
+-- DONE:
+-- ENTITIES:
+-- Pay_slips, Employees, Instructors, Administrators, Managers,
+-- Part_time_Emp, Full_time_Emp, Part_time_instructors, Full_time_instructors,
+-- Customers, Credit_cards, Course_packages,
+-- Courses, Sessions, Offerings,
+--
+-- RELATIONS:
+-- Owns, Buys, Registers, Redeems, Cancels
+
+-- NOT DONE:
+-- ENTITIES:
+-- Course_areas, Rooms
+-- 
+-- RELATIONS
+-- Manages, Handles, Specializes, In, Conducts
+
 CREATE TABLE IF NOT EXISTS Employees (
     eid char(20)    primary key,
     name            text not null,
@@ -61,6 +78,7 @@ CREATE TABLE IF NOT EXISTS Pay_slips (
     amount          integer not null default 0,
     num_work_hours  integer,
     num_work_days   integer,
+	
     primary key     (payment_date, eid),
     foreign key     (eid) references Employees
                     on delete cascade
@@ -118,4 +136,99 @@ CREATE TABLE IF NOT EXISTS Buys (
     primary key (transaction_date, cc_number, package_id),
     foreign key (cc_number) references Owns(cc_number),
     foreign key (package_id) references Course_packages(package_id)
+);
+
+CREATE TABLE IF NOT EXISTS Courses (
+    course_id   char(20)    primary key,
+    title		text     	not null,
+    duration	integer     not null,
+	description	text
+);
+
+CREATE TABLE IF NOT EXISTS Offerings (
+	course_id					char(20),
+    launch_date					date,
+    start_date					date	not null,
+	end_date					date	not null,
+	registration_deadline		date	not null,
+    fees						integer not null,
+    seating_capacity			integer not null,
+    target_number_registrations	integer not null,
+	
+	primary key	(launch_date, course_id),
+	foreign key	(course_id) references Courses
+	
+	constraint within_capacity check (
+		target_number_registrations <= seating_capacity
+	)
+	constraint end_lte_start_date check (
+		start_date <= end_date
+	)
+	constraint register_lte_launch_date check (
+		launch_date <= registration_deadline
+	)
+	constraint start_lte_launch_date check (
+		launch_date <= start_date
+	)
+);
+
+CREATE TABLE IF NOT EXISTS Sessions (
+    sid   			char(20),
+	course_id		char(20),
+	launch_date		date,
+    session_date	date		not null,
+    start_time		integer     not null,
+	end_time		integer		not null, 
+	
+	primary key (sid, course_id, launch_date),
+	foreign key (launch_date, course_id) references Offerings
+	
+	constraint end_lte_start_time check (
+		start_time <= end_time
+	)
+	constraint session_lte_launch_date check (
+		launch_date <= session_date
+	)
+);
+
+CREATE TABLE IF NOT EXISTS Registers (
+    reg_date	date,
+	sid 		char(20),
+	course_id	char(20),
+	launch_date	date,
+    cc_number 	char(20),
+	
+	primary key (reg_date, sid, course_id, launch_date, cc_number),
+	foreign key (sid, course_id, launch_date) references Sessions,
+	foreign key (cc_number) references Owns
+);
+
+CREATE TABLE IF NOT EXISTS Redeems (
+    redeem_date			date,
+	sid 				char(20),
+	course_id			char(20),
+	launch_date			date,
+    transaction_date	date,
+    cc_number           char(20),
+    package_id          char(20),
+	
+	primary key (
+		redeem_date, sid, course_id, launch_date, transaction_date, cc_number, package_id
+	),
+	foreign key (sid, course_id, launch_date) references Sessions,
+	foreign key (transaction_date, cc_number, package_id) references Buys
+);
+
+CREATE TABLE IF NOT EXISTS Cancels (
+    cancel_date		date,
+	sid 			char(20),
+	course_id		char(20),
+	launch_date		date,
+	cust_id 		char(20),
+	
+	primary key (
+		cancel_date, sid, course_id, launch_date, cust_id
+	),
+	foreign key (sid, course_id, launch_date) references Sessions,
+	foreign key (cust_id) references Customers
 );
