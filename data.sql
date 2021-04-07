7d2574ab17b0ebce6dd7ea15c8e30d19eeadabc7
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2021-04-07 23:36:25 +0800

    add more dummy data

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 -- CS2102 Project Team 41 data.sql
 
+DELETE FROM Rooms;
 DELETE FROM Employees;
 DELETE FROM Part_time_Emp;
 DELETE FROM Full_time_Emp;
@@ -7,6 +8,7 @@ DELETE FROM Instructors;
 DELETE FROM Part_time_instructors;
 DELETE FROM Full_time_instructors;
 DELETE FROM Administrators;
+DELETE FROM Course_areas;
 DELETE FROM Managers;
 DELETE FROM Pay_slips;
 DELETE FROM Customers;
@@ -21,8 +23,92 @@ DELETE FROM Registers;
 DELETE FROM Redeems;
 DELETE FROM Cancels;
 
+/*
 INSERT INTO Credit_cards (cc_number, cvv, expiry_date) VALUES
 (1234567891011121, 123, '2013-06-01');
 
 INSERT INTO Customers (cust_name, address, phone, email) VALUES
 ('Joel', 'CCK', '81234567', 'joel@cs2102.com');
+
+    eid             SERIAL primary key,
+    name            text not null,
+    phone           text not null,
+    email           text not null,
+    join_date       date not null,
+    address         text not null,
+    depart_date     date
+*/
+
+INSERT INTO Employees (eid, name, phone, email, join_date, address, depart_date) VALUES
+(1, 'Alice', '90991044', 'alice@cs2102.com', '1999-01-08', '10 Heng Mui Keng Terrace', null),
+(2, 'Bob', '90992242', 'bob@cs2102.com', '1999-01-11', '4 Sandilands Lane', null),
+(3, 'Charlie', '93391069', 'charlie@cs2102.com', '2019-03-25', 'COM1, School Of Computing', null),
+(4, 'Daniel', '99999921', 'daniel@cs2102.com', '2012-04-25', '15 Kent Ridge Drive', null),
+(5, 'Esther', '90881044', 'esther@cs2102.com', '2009-02-08', '10 Heng Mui Keng Terrace', null),
+(6, 'Frodo', '90944242', 'frodo@cs2102.com', '2001-02-11', '4 Sandilands Lane', null),
+(7, 'Gary Manager', '91234554', 'gary@cs2102.com', '2019-03-25', 'The Interlace', null),
+(8, 'Hemanshu Manager', '97126111', 'hemanshu@cs2102.com', '2012-04-27', 'Sentosa Cove', null),
+(9, 'Firzan Manager', '91234114', 'firzan@cs2102.com', '2019-03-25', 'The Interlace', null),
+(10, 'Hodor Manager', '97126001', 'hodor@cs2102.com', '2012-04-27', 'Sentosa Cove', null),
+(11, 'Ivan Administrator', '97126666', 'ivan@cs2102.com', '2012-04-27', 'Sentosa Cove', null);
+
+INSERT INTO Full_time_Emp (eid, monthly_rate) VALUES
+(5, 4000),
+(6, 6000),
+(7, 4000),
+(8, 6000),
+(9, 6900),
+(10, 7000),
+(11, 4000);
+
+INSERT INTO Part_time_Emp (eid, hourly_rate) VALUES
+(1, 69),
+(2, 75),
+(3, 40),
+(4, 120);
+
+INSERT INTO Managers (eid) VALUES
+(7),
+(8),
+(9),
+(10);
+
+INSERT INTO Course_areas (area, eid) VALUES
+('database systems', 7),
+('parallel computing', 8),
+('project management', 9),
+('theoretical physics', 10);
+
+INSERT INTO Instructors (eid, area) VALUES
+(1, 'database systems'),
+(2, 'parallel computing'),
+(3, 'parallel computing'),
+(4, 'database systems'),
+(5, 'project management'),
+(6, 'theoretical physics');
+
+INSERT INTO Part_time_instructors (eid) VALUES
+(1),
+(2),
+(3),
+(4);
+
+INSERT INTO Full_time_instructors (eid) VALUES
+(5),
+(6);
+
+INSERT INTO Administrators (eid) VALUES
+(11);
+
+INSERT INTO Courses (course_id, title, duration, area) VALUES
+(1, 'CS2102: Database Systems', 2, 'database systems'),
+(2, 'CS3223: Database Systems Implementation', 2, 'database systems');
+
+INSERT INTO Offerings (course_id, launch_date, start_date, end_date, registration_deadline, fees, seating_capacity, target_number_registrations, eid) VALUES
+(1, '2021-07-10', '2021-08-10', '2021-11-11', '2021-07-25', 1000, 100, 100, 11);
+
+INSERT INTO Rooms (rid, seating_capacity, location) VALUES
+(1, 100, 'LT15');
+
+INSERT INTO Sessions (sid, course_id, launch_date, session_date, start_time, end_time, rid, eid) VALUES
+(1, 1, '2021-07-10', '2021-08-10', 2, 4, 1, 4);
