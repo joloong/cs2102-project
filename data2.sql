2ead599dd492e4b7c7953e479e4f8dcd875a841d
joloong <joelloong@gmail.com>
2021-04-10 14:10:42 +0800

    Add data using routines

---
+++ data2.sql
@@ -0,0 +1,23 @@
+-- Add to Customers, Credit_cards and Owns tables
+CALL add_customer ('Amy Chen', 'Block 223 Serangoon Avenue 3 #03-42', '93742483', 'amychen@gmail.com', '4566798466523378', '516', '2028-05-03');
+CALL add_customer ('Ben Davids', '1 University Road #01-02', '93753482', 'ben_davids@gmail.com', '4522616717675333', '222', '2024-04-02');
+CALL add_customer ('Charlie Wong', 'Block 13 Woodlands Close #12-33', '89201483', 'charliew@gmail.com', '4535569466151528', '884', '2022-02-05');
+CALL add_customer ('Dawn Lim', 'Block 5 Yishun Street 4 #07-224', '93115481', 'dawn_lim_oo@gmail.com', '4566246626377112', '644', '2026-04-06');
+CALL add_customer ('Evan Ong', 'Block 114 Hougang Street 91 #03-10', '97810030', 'EvanONG@gmail.com', '4670902211105024', '102', '2029-08-07');
+CALL add_customer ('Faizal Ali', 'Block 11 Tampines East Avenue 9 #08-41', '83722420', 'faizalali@gmail.com', '4609801005910630', '351', '2025-07-03');
+CALL add_customer ('Govind Raju', 'Block 9 Serangoon North Avenue 1 #04-01', '89722433', 'govindr@gmail.com', '4612239980177401', '665', '2022-05-02');
+CALL add_customer ('Halimah Wati', 'Block 832 Pasir Ris Street 22 #02-54', '90742284', 'halimahwati@gmail.com', '4999939288503462', '115', '2024-01-06');
+CALL add_customer ('Ivy Lee', '1 Whampoa Street 1 #01-12', '91122485', 'ivysunshine@gmail.com', '4628987119585235', '412', '2028-02-01');
+CALL add_customer ('Johnny Wong', 'Block 33 Yishun Ave 33 #07-123', '81922453', 'johnnywong@gmail.com', '4889029092342005', '143', '2026-03-05');
+
+-- Add to Course_packages
+CALL add_course_package('Package1', 10, 100, '2021-01-01', '2021-12-31');
+CALL add_course_package('Package2', 20, 200, '2021-01-01', '2021-12-31');  
+CALL add_course_package('Package3', 30, 300, '2021-01-01', '2021-12-31');
+CALL add_course_package('Package4', 40, 400, '2021-01-01', '2021-12-31');
+CALL add_course_package('Package5', 50, 500, '2021-01-01', '2021-12-31');  
+CALL add_course_package('Package6', 60, 600, '2021-01-01', '2021-12-31');
+CALL add_course_package('Package7', 70, 700, '2021-01-01', '2021-12-31');
+CALL add_course_package('Package8', 80, 800, '2021-01-01', '2022-12-31');  
+CALL add_course_package('Package9', 90, 900, '2021-01-01', '2021-12-31');
+CALL add_course_package('Package10', 100, 1000, '2021-01-01', '2022-12-31');
