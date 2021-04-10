3177f3d37c145e1d8ddffcc7f4bc0023364a8a90
Firzan Armani <mrfirzanarmani@gmail.com>
2021-04-10 12:20:04 +0800

    Update task list

--- proc.sql
+++ proc.sql
@@ -2,7 +2,7 @@
 
 -- Routine Tracker
 -- Completed/In-Process: 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
--- TODO: 10, 15, 26, 27, 28, 29, 30
+-- TODO: 26, 27, 28, 29, 30
 
 -- 1.
 -- TODO: IF not administrator/manager/instructor
